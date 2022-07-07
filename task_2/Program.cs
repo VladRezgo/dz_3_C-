c79b6ff1dd58e73c8197b9a6f42d4161a32a55b2
«Vlad» <vladislavrezgo@gmail.com>
2022-07-06 23:14:11 -0800

    решение задачи 2

---
+++ task_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+string num1 = Convert.ToString(num);
+
+
+if (num1.Length > 2)
+{
+    if (num1[0] == num1[4] && num1[1] == num1[3])
+    {
+        Console.WriteLine(num1+ "-> число является палиндромом");
+    }
+    else 
+    {
+        Console.WriteLine(num1+ " -> число не является палиндромом");
+    }
+}
