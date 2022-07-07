2d78dcd309d796c44ff8dd2d9306bdb233291b99
«Vlad» <vladislavrezgo@gmail.com>
2022-07-06 23:16:35 -0800

    решение задачи 3

---
+++ task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿// напишите программу, которая принимает на вход число N и выдает таблицу кубов от 1 до N
+
+Console.WriteLine("Введите  число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+
+int n =1;
+int square = 0;
+
+    for (int i = 0; i < num; i++)
+   {
+
+    square = n * n * n;
+    
+    Console.Write("{0}  ",(square));
+    n = n + 1;
+
+       
+   }
